df3afe040dc79692faa6519a60ad10faee178f04
jesse h <jesselevihall@gmail.com>
2022-04-20 20:18:42 -0600

    OAuth 2 facebook

--- routes/users.js
+++ routes/users.js
@@ -9,18 +9,19 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get(
-  "/",
-  authenticate.verifyUser,
-  authenticate.verifyAdmin,
-  (rec, res, next) => {
-    user
-      .find()
-      .then((users) => {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.json(users);
-      })
-      .catch((err) => next(err));
+  "/facebook/token",
+  passport.authenticate("facebook-token"),
+  (req, res) => {
+    if (req.user) {
+      const token = authenticate.getToken({ _id: req.user._id });
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.json({
+        success: true,
+        token: token,
+        status: "You are successfully logged in!",
+      });
+    }
   }
 );
 
